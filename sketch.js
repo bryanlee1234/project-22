dcf99ceaecbfa095383135f9c0de414ce31060ec
bryanlee1234 <77904290+bryanlee1234@users.noreply.github.com>
2021-03-27 21:19:34 -0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -54,11 +54,11 @@ function draw() {
  }
 
 
- if (keyDown(LEFT_ARROW)){  
-	fairy.x = fairy.x - 20;
- }
+ //if (keyDown(LEFT_ARROW)){  
+//	fairy.x = fairy.x - 20;
+// }
 
- if (keyDown(space)){  
+ if (keyDown(LEFT_ARROW)){  
 	Body.setStatic(starBody,false);
  }
 
